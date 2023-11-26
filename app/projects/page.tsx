6a7b705e07c72122657833812ab05c8cbcf9c143
Fahrel <fahrel@students.undip.ac.id>
2023-11-27 01:08:14 +0700

    feat: add projects page

---
+++ app/projects/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <>
+      <main className="bg-gray-700 text-white h-screen text-center">
+        <p className="animate-pulse hover:animate-spin">Ongoing lmao</p>
+      </main>
+    </>
+  );
+}
